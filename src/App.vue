bed9e907faaf08b55d70229c8d54fb4a1385461e
Bagas Prabowo <bagasprabowo2412@gmail.com>
2024-09-25 00:51:06 +0700

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <header class="flex justify-between items-center max-h-20 px-32 py-8">
+    <img src="./assets/img/Brand.png" alt="brand img" class="max-w-[117.54px] max-h-[28px]">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <ul class="flex space-x-4">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+
+    <button class="bg-blue-500 text-white font-bold px-8 py-3 rounded-[63px]">
+      Contact Us
+    </button>
   </header>
 
   <main>
-    <TheWelcome />
+
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
