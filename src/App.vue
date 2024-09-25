4ece0a3e50fce147fadc602392459f4ad8f92e62
Bagas Prabowo <bagasprabowo2412@gmail.com>
2024-09-25 22:13:08 +0700

    create frame content-1 & fix bug in header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <script setup>
+import ContentFrame1 from './components/ContentFrame1.vue';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header class="flex justify-between items-center max-h-20 px-32 py-8">
-    <img src="./assets/img/Brand.png" alt="brand img" class="max-w-[117.54px] max-h-[28px]">
+  <header class="h-20">
+    <div class="flex justify-between items-center max-h-[80px] px-32 py-16">
+      <img src="./assets/img/Brand.png" alt="brand img" class="max-w-[117.54px] max-h-[28px]">
 
-    <ul class="flex space-x-4">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Blog</a></li>
-      <li><a href="#">About Us</a></li>
-      <li><a href="#">Contact Us</a></li>
-    </ul>
+      <ul class="flex space-x-4">
+        <li class="font sans text-[16px] leading-7"><a href="#">Home</a></li>
+        <li class="font sans text-[16px] leading-7"><a href="#">Blog</a></li>
+        <li class="font sans text-[16px] leading-7"><a href="#">About Us</a></li>
+        <li class="font sans text-[16px] leading-7"><a href="#">Contact Us</a></li>
+      </ul>
 
-    <button class="bg-[#075EFD] text-white font-bold px-8 py-3 rounded-[63px]">
-      Contact Us
-    </button>
+      <button class="bg-[#075EFD] text-white font-bold px-8 py-3 rounded-[63px]">
+        Contact Us
+      </button>
+    </div>
+
   </header>
 
   <main>
-
+    <ContentFrame1 />
   </main>
 </template>
 
---
+++ src/components/ContentFrame1.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="flex justify-between items-center px-32 py-16">
+        <!-- <div class="grid grid-cols-2 space-x-32 items-center"> -->
+        <div class="w-[616px] gap-8">  
+            <p class="font-sans font-semibold text-[16px] leading-5 tracking-[3px] text-[#FACA15]">
+                CAREER ACELERATE
+            </p>
+            <p class="font-sans font-semibold mt-10 w-[616px] h-[128px] text-[48px] leading-16">
+                Meningkatkan Karir Anda di SISTEM Bootcamp
+            </p>
+            <p class="font-sans font-normal mt-10 h-[56px] text-[16px] leading-7">
+                Pelajari dari ahli Teknik Perangkat Lunak yang berpengalaman di SEA dan mempercepat karir Anda sebagai Software Engineer.
+            </p>
+
+            <div class="grid grid-cols-3 gap-12 mt-10">
+                <div class="flex justify-between items-center space-x-4">
+                    <img src="../assets/icon/icon.png" width="64px" height="64px" alt="">
+                    <p class="font-poppins font-semibold text-[18px] leading-[27px]">
+                        Build Community
+                    </p>
+                </div>
+                <div class="flex justify-between items-center space-x-4">
+                    <img src="../assets/icon/icon (2).png" width="64px" height="64px" alt="">
+                    <p class="font-poppins font-semibold text-[18px] leading-[27px]">
+                        Share Events
+                    </p>
+                </div>
+                <div class="flex justify-between items-center space-x-4">
+                    <img src="../assets/icon/icon (3).png" width="64px" height="64px" alt="">
+                    <p class="font-poppins font-semibold text-[18px] leading-[27px]">
+                        Get Information
+                    </p>
+                </div>
+            </div>
+
+            <button class="bg-[#075EFD] text-white font-bold px-8 py-3 mt-10 rounded-[63px]">
+                Contact Us
+            </button>
+        </div>
+        
+        <div class="">
+            <img src="../assets/img/Group 3.png" alt="" class="max-w-[421.11px] max-h-[419.5px]">
+        </div>
+        <!-- </div> -->
+        
+    </section>
+</template>
